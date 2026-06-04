ed51a848c02c23ce6185b1cbbaa65ad494f5056a
hendri <heksatria@gmail.com>
2026-06-04 11:28:03 +0700

    update 08-slice.go

--- 08-slice.go
+++ 08-slice.go
@@ -6,24 +6,32 @@ func latSlice(){
 	var buah = [...]string{"jambu","apel","pepaya","klengkeng","rambutan"}
 
 	//Panjang array
-	fmt.Println(len(buah)) //5
+	fmt.Println("Panjang array buah : ",len(buah)) //5
 
 	var slice1 = buah[1:3]
-	fmt.Println(slice1)  //[apel pepaya]
+	fmt.Println("Panjang slice-1 : ",len(slice1))  //[apel pepaya]
+	fmt.Println("Kapasitas slice-1 : ", cap(slice1))  // apel sampai index array buah terakhir
+	fmt.Println("Isi slice-1 : ",slice1)  //[apel pepaya]
 
 	//Slice adalah pointer ke array, buktinya
 	slice1[0]= "pir"
-	fmt.Println(slice1)  //[pir pepaya]
+	fmt.Println("Isi array buah sekarang :  ",buah)  //[pir pepaya]
 	
 	//Menambah isi slice
 	// append(slice1,"kelapa") //(value of type []string) is not used --> panjang array cuma 5
 	//Solusi => buat slice baru
 	var slice2 = append(slice1,"kelapa")
-	fmt.Println(slice2)
+	fmt.Println("Kapasitas slice-2 : ",cap(slice2))
+	fmt.Println("Isi slice-2 : ",slice2)
 
 	slice2 = append(slice2,"kelapa2")
 	slice2 = append(slice2,"kelapa3")
 	slice2 = append(slice2,"kelapa4")
+	slice2 = append(slice2,"kelapa5")
+	fmt.Println(slice2)
+	fmt.Println(cap(slice2))
+	slice2 = append(slice2,"kelapa6") //Otomatis kapasitas slice ditambah jadi 2 kali lipat
+	slice2 = append(slice2,"kelapa7")
 	fmt.Println(slice2)
 	fmt.Println(cap(slice2))
 
